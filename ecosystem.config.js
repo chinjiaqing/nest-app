785c3a722b0f32629a49dff4bedf317b437a7bad
chinjiaqing <chinjiaqing@qq.com>
2025-04-28 15:19:45 +0800

    fix: 修改pm2配置

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,18 +5,20 @@ module.exports = {
             exec_mode: 'cluster',
             autorestart: true,
             watch: false,
-            // max_memory_restart: '1G',
+            max_memory_restart: '500M',
             env_test: {
                 NODE_ENV: 'test',
                 name: 'nest-app-test',
                 instances: 1,
-                PORT: 2001
+                PORT: 2001,
+                TZ: 'Asia/Shanghai'
             },
             env_prod: {
                 NODE_ENV: 'prod',
                 name: 'nest-app-prod',
                 PORT: 2002,
                 instances: 1,
+                TZ: 'Asia/Shanghai'
             }
         },
     ],
